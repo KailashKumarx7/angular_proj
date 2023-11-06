791544a380431395ca6b3e73475c33ebf7b2ec0d
kailash <Thekailashkumar@outlook.com>
2023-11-06 15:55:45 +0545

    1.2

--- src/app/components/test-component/test-component.component.ts
+++ src/app/components/test-component/test-component.component.ts
@@ -1,33 +1,32 @@
-import { Component, ElementRef, Inject, OnInit } from '@angular/core';
-import { GetDataService } from 'src/app/services/get-data.service';
-import { PrintService } from 'src/app/services/print.service';
-import { NepaliDatepickerService } from 'nepali-datepicker-angular';
+import { Component, OnInit } from "@angular/core";
 
 
-export interface Transaction {
-  item: string;
-  cost: number;
-}
 @Component({
   selector: 'app-test-component',
   templateUrl: './test-component.component.html',
   styleUrls: ['./test-component.component.scss']
 })
-export class TestComponentComponent implements OnInit{
-  date:string='';
-  constructor(
-    private _nepaliDatepickerService: NepaliDatepickerService,
+export class TestComponentComponent implements OnInit {
+  constructor() { }
 
-  ){
-    
-  }
-  ngOnInit(): void {
-   
-  }
+  ngOnInit() {
+    // Detect connected devices
+    navigator.mediaDevices.enumerateDevices()
+      .then(devices => {
+        for (const device of devices) {
+          const deviceInfo = {
+            type: device.kind,
+            operatingSystem: device.label,
+            browser: navigator.userAgent,
+            screenResolution: window.screen.width + 'x' + window.screen.height
+          };
 
-  startingDate($event:any){
-
+          // Do something with the device info
+          console.log(deviceInfo);
+        }
+      })
+      .catch(error => {
+        console.error('Error enumerating devices:', error);
+      });
   }
-    
-  }
-
+}
